99fc686927cdf04b72ceb0eb6518c35f739870b8
SAJAD AHMAD RATHER <46589981+SajadAHMAD1@users.noreply.github.com>
2022-11-18 12:26:25 +0530

    Add files via upload

---
+++ Func_version_of_min_index_in_a_list.py
@@ -0,0 +1,41 @@
+
+"""
+Program: To find the index of the minimum value of the element in the list
+Programmer: Dr. Sajad Ahmad Rather
+"""
+
+def min_index(x):
+
+    sorted_x = sorted(x)
+    #print(sorted_x)
+
+    for temp in range(len(x)):
+        num = x[temp]
+        q = sorted_x[0]
+        if num == q:
+            # print(f'Index of the minimum value {num} is: {temp}')
+            return num, temp
+
+def show_min_index(y):
+    return f'Index of the minimum value is : {y}'
+
+
+
+#Sample_example = [400,45,20,33,22]
+#list = [400,45,20,33,22]
+
+Num_of_elements = int(input('How many values you want in a list: '))
+
+list = []
+for temp_1 in range(Num_of_elements):
+    number = int(input('Enter a number: '))
+    list.append(number)
+print(f'Your entered list is: {list}')
+
+l = min_index(list)
+print(l)
+
+c = l [1]
+
+m = show_min_index(c)
+print(m)
